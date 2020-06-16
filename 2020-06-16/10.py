f2265e21b7f296e02b77c1f7a7dae1bc3dcf772f
gry-kiu <65639145+gry-kiu@users.noreply.github.com>
2020-06-17 08:49:35 +0900

    Add files via upload

---
+++ 2020-06-16/10.py
@@ -0,0 +1,4 @@
+# 파일을 엽니다.
+with open("basic.txt", "w") as file:
+    # 파일에 텍스트를 씁니다.
+    file.write("Hello Python Programming...!")
---
+++ 2020-06-16/15.py
@@ -0,0 +1,23 @@
+# 함수를 선언합니다.
+def test():
+    print("A 지점 통과")
+    yield 1
+    print("B 지점 통과")
+    yield 2
+    print("C 지점 통과")
+
+# 함수를 호출합니다.
+output = test()
+
+# next() 함수를 호출합니다.
+print("D 지점 통과")
+a = next(output)
+print(a)
+
+print("E 지점 통과")
+b = next(output)
+print(b)
+
+print("F 지점 통과")
+c = next(output)
+print(c)
